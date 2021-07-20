bc5e8ed5ea5cdac0a22d3decac4c3568666fcc1c
Anders Olson <sredna43@gmail.com>
2021-07-20 09:14:31 -0500

    trying command line args for port, working

--- scenes/main/DedicatedServer.gd
+++ scenes/main/DedicatedServer.gd
@@ -46,8 +46,6 @@ func _ready():
 		if argument.find("=") > -1:
 			var key_value = argument.split("=")
 			arguments[key_value[0].lstrip("--")] = key_value[1]
-	print(arguments)
-	print(OS.get_cmdline_args())
 	if arguments.has("port"):
 		port = arguments["port"]
 	start_server()
